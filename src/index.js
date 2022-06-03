6bf3603bb1377505feeebd77705ae572eda23fb6
juanmabascu997 <jmb972012@gmail.com>
2022-06-03 14:29:34 -0300

    Bsad

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-//import { Provider } from "react-redux";
 import store from "./store/index";
 
 ReactDOM.render(
